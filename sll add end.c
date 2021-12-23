0c288ae2b288fdc6cc7ccd835c69a8c627542ea3
MG-1410 <muthujeganatham@gmail.com>
2021-12-23 12:32:59 +0530

    Single Linked List Adding Elements at end

---
+++ sll add end.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int i;
+    struct node *link;
+};
+
+struct node* add_at_end(struct node *ptr , int data)
+{
+    struct node *temp= (struct node *)malloc(sizeof(struct node));
+    temp->i=data;
+    temp->link=NULL;
+    ptr->link=temp;
+    return temp;
+}
+
+int main()
+{
+    struct node *head= (struct node *)malloc(sizeof(struct node));
+    head->i=14;
+    head->link=NULL;
+    struct node  *ptr=head;
+    ptr=add_at_end(ptr,28);
+    ptr=add_at_end(ptr,42);
+    ptr=add_at_end(ptr,56);
+
+    ptr=head;
+    while (ptr!=NULL)
+    {
+        printf("%d ",ptr->i);
+        ptr=ptr->link;
+    }
+}
